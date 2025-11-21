d1dbab18ea73f5c49a807ee0ee19a49fc03bed8f
feliprex234 <feliprex234@gmail.com>
2025-11-21 20:52:26 -0300

    Nova Classe SessaoUsuario pra armazenar o login

---
+++ NommusProject/NommusProject/SessaoUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NommusProject
+{
+    public static class SessaoUsuario
+    {
+        public static Usuario UsuarioLogado { get; set; }
+
+        public static bool EstaLogado => UsuarioLogado != null;
+
+        public static void Logout()
+        {
+            UsuarioLogado = null;
+        }
+
+        public static bool IsPremium()
+        {
+            return EstaLogado && UsuarioLogado.Tipo == TipoUsuario.Premium;
+        }
+
+        public static bool IsAdm()
+        {
+            return EstaLogado && UsuarioLogado.Tipo == TipoUsuario.Adm;
+        }
+    }
+}
