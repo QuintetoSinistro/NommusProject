d45f8bb6eb32f331f16fc819cdfbaa2a9ab77064
feliprex234 <feliprex234@gmail.com>
2025-11-24 12:46:15 -0300

    Separação e inserção de Icone

--- NommusProject/NommusProject/Models/CreditCard.cs
+++ NommusProject/NommusProject/Models/CreditCard.cs
@@ -1,14 +1,12 @@
-namespace Nommus
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NommusProject
 {
-    public class CreditCard
+    internal class CreditCard
     {
-        public int Id { get; set; }
-        public string BankName { get; set; }
-        public string CardType { get; set; }
-        public string CardNumber { get; set; }
-        public string CardHolder { get; set; }
-        public string ExpiryDate { get; set; }
-        public string CVV { get; set; }
-        public System.Windows.Media.Brush CardColor { get; set; }
     }
 }
